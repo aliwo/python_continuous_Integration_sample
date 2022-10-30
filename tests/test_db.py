59730f40e450c905dd99896821cde6d99a1b671e
SW <aliwo@naver.com>
2022-10-30 12:44:25 +0900

    run black

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,17 +4,10 @@ from database.memo import Memo
 
 
 class TestDB(TestCase):
-
     async def test_all_은_모든_레코드를_가져온다(self) -> None:
         # Given
-        await Memo.create(
-            title="test 제목",
-            body="test 본문"
-        )
-        await Memo.create(
-            title="test 제목2",
-            body="test 본문2"
-        )
+        await Memo.create(title="test 제목", body="test 본문")
+        await Memo.create(title="test 제목2", body="test 본문2")
 
         # When
         memos = await Memo.all()
