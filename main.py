e73b05b62d762242365e1bf8b1ebcfbc3483cac7
SW <aliwo@naver.com>
2022-10-30 18:37:10 +0900

    single quote

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class MemoOut(BaseModel):
     body: str
 
 
-@app.get("/memos")
+@app.get('/memos')  # 모두가 double quote 를 쓰지만 나는 single quote 쓰고 싶은데?
 async def get_all_memos() -> list[MemoOut]:
     memos = await Memo.all()
     return [MemoOut(title=memo.title, body=memo.body) for memo in memos]
